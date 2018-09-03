c72a6d7daeb98cec071c6a0f659113dc471864ee
resourceldg <32602897+resourceldg@users.noreply.github.com>
2018-09-03 12:03:09 -0300

    Update forms.py

--- bitnat/forms.py
+++ bitnat/forms.py
@@ -11,5 +11,5 @@ class Formulario_Contacto(forms.Form):
     widget=(forms.TextInput(attrs={"value": ""})))
     fempresa = forms.CharField(max_length=70, required=False, label="empresa",
     widget=(forms.TextInput(attrs={"value": ""})))
-    fmensaje = forms.CharField(max_length=500, required=True, label="mensaje",
+    fmensaje = forms.CharField(max_length=500, required=False, label="mensaje",
     widget=(forms.Textarea(attrs={"value": ""})))
