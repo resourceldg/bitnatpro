35c0193e207ac3315b6abf3c77b1a2056ccbf4d3
resourceldg <resourceldg@gmail.com>
2019-01-24 11:36:16 -0300

    uhhh

--- bitnat/views.py
+++ bitnat/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
@@ -60,3 +61,322 @@ class Contact(View):
 
 def gracias(request):
     return render(request, 'gracias.html')
+
+def sitemap(request):
+    return render(request, 'sitemap.xml')
+
+def robots(request):
+    return render(request, 'robots.txt')
+
+#dry(dont repeat yourself): me estoy repitiendo grrrr.(SOLUCIONAR LAS SIGUIENTE VISTAS GENERERICAS)
+#APRENDER A USAR DECORADORES, MIXINS HERENCIAS DE VISTAS GENERICAS Y DE MODELOS
+
+class Social_lp(View):
+    form_class = Formulario_Contacto
+    initial = {'key': 'value'}
+    template_name = 'ventas-en-redes-sociales.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                datos = form.cleaned_data
+                fnombre = datos.get("fnombre")
+                femail = datos.get("femail")
+                ftelefono = datos.get("ftelefono")
+                fempresa = datos.get("fempresa")
+                fmensaje = datos.get("fmensaje")
+                db_register = Contacto(nombre=fnombre, email=femail,telefono=ftelefono,empresa=fempresa,mensaje=fmensaje)
+                db_register.save()
+                body = render_to_string(
+                       'email_content.html', {
+                            'fnombre': fnombre,
+                            'fmensaje': fmensaje,
+                            'femail': femail,
+                            },
+                        )
+                email_message = EmailMessage(
+                    subject='mensaje de usuario',
+                    body=body,
+                    from_email=settings.EMAIL_HOST_USER,
+                    to=[settings.EMAIL_HOST_USER],
+                )
+                email_message.content_subtype = 'html'
+                email_message.send()
+                return HttpResponseRedirect('gracias', {'form': form})
+
+        else:
+            form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+
+class Negocio_lp(View):
+    form_class = Formulario_Contacto
+    initial = {'key': 'value'}
+    template_name = 'negocios-digitales.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                datos = form.cleaned_data
+                fnombre = datos.get("fnombre")
+                femail = datos.get("femail")
+                ftelefono = datos.get("ftelefono")
+                fempresa = datos.get("fempresa")
+                fmensaje = datos.get("fmensaje")
+                db_register = Contacto(nombre=fnombre, email=femail,telefono=ftelefono,empresa=fempresa,mensaje=fmensaje)
+                db_register.save()
+                body = render_to_string(
+                       'email_content.html', {
+                            'fnombre': fnombre,
+                            'fmensaje': fmensaje,
+                            'femail': femail,
+                            },
+                        )
+                email_message = EmailMessage(
+                    subject='mensaje de usuario',
+                    body=body,
+                    from_email=settings.EMAIL_HOST_USER,
+                    to=[settings.EMAIL_HOST_USER],
+                )
+                email_message.content_subtype = 'html'
+                email_message.send()
+                return HttpResponseRedirect('gracias', {'form': form})
+
+        else:
+            form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+
+class Analytics_lp(View):
+    form_class = Formulario_Contacto
+    initial = {'key': 'value'}
+    template_name = 'analitica-web.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                datos = form.cleaned_data
+                fnombre = datos.get("fnombre")
+                femail = datos.get("femail")
+                ftelefono = datos.get("ftelefono")
+                fempresa = datos.get("fempresa")
+                fmensaje = datos.get("fmensaje")
+                db_register = Contacto(nombre=fnombre, email=femail,telefono=ftelefono,empresa=fempresa,mensaje=fmensaje)
+                db_register.save()
+                body = render_to_string(
+                       'email_content.html', {
+                            'fnombre': fnombre,
+                            'fmensaje': fmensaje,
+                            'femail': femail,
+                            },
+                        )
+                email_message = EmailMessage(
+                    subject='mensaje de usuario',
+                    body=body,
+                    from_email=settings.EMAIL_HOST_USER,
+                    to=[settings.EMAIL_HOST_USER],
+                )
+                email_message.content_subtype = 'html'
+                email_message.send()
+                return HttpResponseRedirect('gracias', {'form': form})
+
+        else:
+            form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+
+
+class Contenido_lp(View):
+    form_class = Formulario_Contacto
+    initial = {'key': 'value'}
+    template_name = 'marketing-de-contenidos.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                datos = form.cleaned_data
+                fnombre = datos.get("fnombre")
+                femail = datos.get("femail")
+                ftelefono = datos.get("ftelefono")
+                fempresa = datos.get("fempresa")
+                fmensaje = datos.get("fmensaje")
+                db_register = Contacto(nombre=fnombre, email=femail,telefono=ftelefono,empresa=fempresa,mensaje=fmensaje)
+                db_register.save()
+                body = render_to_string(
+                       'email_content.html', {
+                            'fnombre': fnombre,
+                            'fmensaje': fmensaje,
+                            'femail': femail,
+                            },
+                        )
+                email_message = EmailMessage(
+                    subject='mensaje de usuario',
+                    body=body,
+                    from_email=settings.EMAIL_HOST_USER,
+                    to=[settings.EMAIL_HOST_USER],
+                )
+                email_message.content_subtype = 'html'
+                email_message.send()
+                return HttpResponseRedirect('gracias', {'form': form})
+
+        else:
+            form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+
+
+class Posicionamiento_web(View):
+    form_class = Formulario_Contacto
+    initial = {'key': 'value'}
+    template_name = 'posicionamiento-web.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                datos = form.cleaned_data
+                fnombre = datos.get("fnombre")
+                femail = datos.get("femail")
+                ftelefono = datos.get("ftelefono")
+                fempresa = datos.get("fempresa")
+                fmensaje = datos.get("fmensaje")
+                db_register = Contacto(nombre=fnombre, email=femail,telefono=ftelefono,empresa=fempresa,mensaje=fmensaje)
+                db_register.save()
+                body = render_to_string(
+                       'email_content.html', {
+                            'fnombre': fnombre,
+                            'fmensaje': fmensaje,
+                            'femail': femail,
+                            },
+                        )
+                email_message = EmailMessage(
+                    subject='mensaje de usuario',
+                    body=body,
+                    from_email=settings.EMAIL_HOST_USER,
+                    to=[settings.EMAIL_HOST_USER],
+                )
+                email_message.content_subtype = 'html'
+                email_message.send()
+                return HttpResponseRedirect('gracias', {'form': form})
+
+        else:
+            form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+
+
+class Emailmkt_lp(View):
+    form_class = Formulario_Contacto
+    initial = {'key': 'value'}
+    template_name = 'email-marketing.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                datos = form.cleaned_data
+                fnombre = datos.get("fnombre")
+                femail = datos.get("femail")
+                ftelefono = datos.get("ftelefono")
+                fempresa = datos.get("fempresa")
+                fmensaje = datos.get("fmensaje")
+                db_register = Contacto(nombre=fnombre, email=femail,telefono=ftelefono,empresa=fempresa,mensaje=fmensaje)
+                db_register.save()
+                body = render_to_string(
+                       'email_content.html', {
+                            'fnombre': fnombre,
+                            'fmensaje': fmensaje,
+                            'femail': femail,
+                            },
+                        )
+                email_message = EmailMessage(
+                    subject='mensaje de usuario',
+                    body=body,
+                    from_email=settings.EMAIL_HOST_USER,
+                    to=[settings.EMAIL_HOST_USER],
+                )
+                email_message.content_subtype = 'html'
+                email_message.send()
+                return HttpResponseRedirect('gracias', {'form': form})
+
+        else:
+            form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+
+
+class Ctalider_lp(View):
+    form_class = Formulario_Contacto
+    initial = {'key': 'value'}
+    template_name = 'asesoria-gratuita-en-marketing-digital.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                datos = form.cleaned_data
+                fnombre = datos.get("fnombre")
+                femail = datos.get("femail")
+                ftelefono = datos.get("ftelefono")
+                fempresa = datos.get("fempresa")
+                fmensaje = datos.get("fmensaje")
+                db_register = Contacto(nombre=fnombre, email=femail,telefono=ftelefono,empresa=fempresa,mensaje=fmensaje)
+                db_register.save()
+                body = render_to_string(
+                       'email_content.html', {
+                            'fnombre': fnombre,
+                            'fmensaje': fmensaje,
+                            'femail': femail,
+                            },
+                        )
+                email_message = EmailMessage(
+                    subject='mensaje de usuario',
+                    body=body,
+                    from_email=settings.EMAIL_HOST_USER,
+                    to=[settings.EMAIL_HOST_USER],
+                )
+                email_message.content_subtype = 'html'
+                email_message.send()
+                return HttpResponseRedirect('gracias', {'form': form})
+
+        else:
+            form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+
+
+
+
+
--- btn/urls.py
+++ btn/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """bn URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -54,10 +55,29 @@ urlpatterns = [
 
     #bitnativo.com
 
+
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.inicio, name='inicio'),
     url(r'^contact/$', views.Contact.as_view(), name='Contact'),
-    url(r'^contact/gracias/', views.gracias, name='gracias'),
+#    url(r'^sitemap/$', views.sitemap, name='sitemap'),
+ #   url(r'^robots.txt/$', views.robots, name='robots'),
+    url(r'^contact/gracias/$', views.gracias, name='gracias'),
+    url(r'^ventas-en-redes-sociales/gracias/$', views.gracias, name='gracias'),#dry(dont repeat yourself): me estoy repitiendo (SOLUCION: APRENDER A USAR REGEX EN PATRONES URL)
+    url(r'^negocios-digitales/gracias/$', views.gracias, name='gracias'),#dry(dont repeat yourself): me estoy repitiendo (SOLUCION: APRENDER A USAR REGEX EN PATRONES URL)
+    url(r'^analitica-web/gracias/$', views.gracias, name='gracias'),#dry(dont repeat yourself): me estoy repitiendo (SOLUCION: APRENDER A USAR REGEX EN PATRONES URL)
+    url(r'^marketing-de-contenidos/gracias/$', views.gracias, name='gracias'),#dry(dont repeat yourself): me estoy repitiendo (SOLUCION: APRENDER A USAR REGEX EN PATRONES URL)
+    url(r'^posicionamiento-web/gracias/$', views.gracias, name='gracias'),#dry(dont repeat yourself): me estoy repitiendo (SOLUCION: APRENDER A USAR REGEX EN PATRONES URL)
+    url(r'^email-marketing/gracias/$', views.gracias, name='gracias'),#dry(dont repeat yourself): me estoy repitiendo (SOLUCION: APRENDER A USAR REGEX EN PATRONES URL)
+    url(r'^asesoria-gratuita-en-marketing-digital/gracias/$', views.gracias, name='gracias'),#dry(dont repeat yourself): me estoy repitiendo (SOLUCION: APRENDER A USAR REGEX EN PATRONES URL)
+#LANDING PAGES
+    url(r'^ventas-en-redes-sociales/$', views.Social_lp.as_view(), name='ventas-en-redes-sociales'),
+    url(r'^negocios-digitales/$', views.Negocio_lp.as_view(), name='negocios-digitales'),
+    url(r'^analitica-web/$', views.Analytics_lp.as_view(), name='analitica-web'),
+    url(r'^marketing-de-contenidos/$', views.Contenido_lp.as_view(), name='marketing-de-contenidos'),
+    url(r'^posicionamiento-web/$', views.Posicionamiento_web.as_view(), name='posicionamiento-web'),
+    url(r'^email-marketing/$', views.Emailmkt_lp.as_view(), name='email-marketing'),
+    url(r'^asesoria-gratuita-en-marketing-digital/$', views.Ctalider_lp.as_view(), name='asesoria-gratuita-en-marketing-digital'),
+#FIN LANDING PAGES
 
 
 
@@ -68,7 +88,6 @@ urlpatterns = [
     url(r'^comments/', include('django_comments.urls')),
 
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
